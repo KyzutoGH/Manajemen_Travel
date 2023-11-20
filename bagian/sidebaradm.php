f401c1bbae79f58c5423a9fea6d74b3279c7b1ca
KyzutoGH - Muhammad Ikhsan <iksancjr@gmail.com>
2023-11-20 11:33:56 +0700

    Penyederhanaan Kode

--- bagian/sidebaradm.php
+++ bagian/sidebaradm.php
@@ -33,9 +33,57 @@
 						</a>
 					</li>
 				</ul>
+			</li>
+			<li class="treeview <?php if ($menu == "Pelanggan") {
+									echo "active";
+								} ?>">
+				<a href="#">
+					<i class="fa fa-user" style="color: skyblue;"></i><span>Pelanggan</span>
+					<span class="pull-right-container">
+						<i class="fa fa-angle-left pull-right"></i>
+					</span>
+				</a>
 				<ul class="treeview-menu">
-					<li></li>
+					<li class="<?php if ($submenu == "Pelanggan") {
+									echo "active";
+								} ?>">
+						<a href="<?php
+									$subzero = isset($_GET['subzero']) ? $_GET['subzero'] : '';
+									echo isset($subzero) && $subzero == "Pelanggan" ? '..' : '.';
+									?>/pelanggan.php">
+							<i class="fa fa-bus text-aqua"></i> Data Pelanggan
+						</a>
+					</li>
+					<li class="<?php if ($submenu == "Penumpang") {
+									echo "active";
+								} ?>">
+						<a href="<?php
+									$subzero = isset($_GET['subzero']) ? $_GET['subzero'] : '';
+									echo isset($subzero) && $subzero == "Penumpang" ? '..' : '.';
+									?>/penumpang.php">
+							<i class="fa fa-table text-red"></i> Data Penumpang
+						</a>
+					</li>
 				</ul>
+			<li class="<?php if ($submenu == "Transaksi") {
+							echo "active";
+						} ?>">
+				<a href="<?php
+							$subzero = isset($_GET['subzero']) ? $_GET['subzero'] : '';
+							echo isset($subzero) && $subzero == "Transaksi" ? '..' : '.';
+							?>/transaksi.php">
+					<i class="fa fa-money text-green"></i>Transaksi
+				</a>
+			</li><li class="<?php if ($submenu == "Laporan") {
+							echo "active";
+						} ?>">
+				<a href="<?php
+							$subzero = isset($_GET['subzero']) ? $_GET['subzero'] : '';
+							echo isset($subzero) && $subzero == "Laporan" ? '..' : '.';
+							?>/laporan.php">
+					<i class="fa fa-print text-yellow"></i>Laporan
+				</a>
+			</li>
 			</li>
 		</ul>
 	</section>
