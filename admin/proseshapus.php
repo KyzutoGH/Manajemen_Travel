052f5b4b391410e9689cd792d04308723e40ec06
KyzutoGH <iksancjr@gmail.com>
2025-10-27 14:41:35 +0700

    FIx

--- admin/proseshapus.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  # code...
-    $id = $_GET['id_buku'];
-    mysqli_query($koneksi,"DELETE FROM data_buku WHERE id_buku='$id'");
-
-    header("location:index.php");
-?>
--- admin/prosesuser.php
+++
@@ -1,6 +0,0 @@
-    <?php
-    $id = $_GET['id'];
-    mysqli_query($koneksi,"DELETE FROM user WHERE id='$id'");
-
-    header("location:edituser.php");
-?>
